d1ad0fe4b802ca770665c204e2034ecfa79df366
mdsaidul44 <islam88758@gmail.com>
2024-03-04 21:01:03 +0600

    button click added

--- js/app.js
+++ js/app.js
@@ -3,26 +3,27 @@ const loadData = async (searchText) => {
     const data = await res.json()
     const allData = data.posts
     showDisplayData(allData) 
+    
 }
  
-
-
+  
 const showDisplayData = (items) => {
     
     const newsCardContainer = document.getElementById('card-section')
+    
     newsCardContainer.innerHTML = ''
     // console.log( items)
     items.forEach((item) => {
-        console.log(item)
-         let countRead = ''
-         if(item.author.isActive){
-            countRead=`green`
-         }
+        console.log(item)  
+         let countRead = '${item.author.isActive }'
+          let red = countRead==true ? "rad":"green";
+
         const newsCard = document.createElement('div')
         newsCard.innerHTML = `
         <div class="bg-slate-300 lg:flex lg:w-[800px] rounded-2xl border-slate-400 border-2 mt-6">
                             <div class="indicator w-16 h-20 m-4 mt-10">
-                            <span class="indicator-item badge">${countRead}</span>
+                            
+                            <span class="indicator-item badge">${red}</span>
                                 <img src="${item.image}" alt="">
                             </div>
                             <div class="py-8 px-2">
@@ -49,36 +50,39 @@ const showDisplayData = (items) => {
                                         <span>${item.posted_time}</span>
                                     </div>
                                     <div class="lg:ml-80">
-                                        <button onclick="handleButton()" class="bg-sky-400 w-10 h-10 rounded-full"><i
-                                                class="fa-solid fa-envelope-open-text "></i></button>
+                                        <button id="news-btn" onclick="handleButton(${item?.author?.title},${item.author?.view_count})" class="bg-sky-400 w-10 h-10 rounded-full"><i
+                                        class="fa-solid fa-envelope-open-text "></i></button>
                                     </div>
                                 </div>
                             </div>
                         </div>
         `
         newsCardContainer.appendChild(newsCard) 
-    })
+ 
+    }) 
     toggleLoadingSpinner(false)
 }
-
  
+
 // const countReading = document.getElementById('count-reading')
 const readContainer = document.getElementById('read-section')
-const handleButton = () => { 
-      
+const handleButton = (click,view)=> {  
     console.log('button added')
     const readSection = document.createElement('div')
     readSection.innerHTML = `
     <div class="flex justify-between  bg-slate-100 mx-4 rounded-xl mb-2 p-4">
-        <h1 class=" font-semibold w-72">10 Kids Unaware of Their Halloween Costume</h1>
+        <h1 class=" font-semibold w-72">Introduction to Python: A Beginner's Guide</h1>
         <div class="mt-2">
             <i class="fa-regular fa-eye mt-1.5 "></i>
-            <span>560</span>
+            <span>${click}</span>
         </div>
     </div>
     `
-    readContainer.appendChild(readSection)
+    readContainer.appendChild(readSection) 
+
 }
+
+
 
 const searchHandler = () => {
     toggleLoadingSpinner(true)
@@ -105,7 +109,7 @@ const uploadData = async () => {
         <div class="">
             <div class="flex ">
                 <i class="fa-solid fa-briefcase m-1.5"></i>
-                <p>${data.author?.posted_date}</p>
+                <p>${data.author.posted_date}</p>
             </div>
             <h1 class="font-bold mb-3">${data.title}</h1>
             <p>${data.description}</p>
